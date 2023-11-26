cab278c613d5a29cbade48add813ff8d46f77608
AuralinACC <robert.veihman@g.austincc.edu>
2023-11-26 00:14:25 -0600

    Moved slash command logic to SlashCommands.lua

---
+++ SlashCommands.lua
@@ -0,0 +1,9 @@
+-- Add a slash command for showing/hiding the options menu
+SLASH_AVP1 = "/avp"
+SlashCmdList["AVP"] = function()
+    if MainMenuFrame:IsShown() then
+        MainMenuFrame:Hide()
+    else
+        MainMenuFrame:Show()
+    end
+end
